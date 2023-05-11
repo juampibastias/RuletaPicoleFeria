fc3aa7ee1748788d224ff08fe19769259891b799
lucas-escolasorvete <lucas@escolasorvete.com.br>
2023-05-11 16:03:02 -0300

    Adding styles to form page before wheel

--- src/FormPage.js
+++ src/FormPage.js
@@ -1,87 +1,100 @@
-import React, { useState } from "react";
-import axios from "axios";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState } from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './FormPage.css';
+import Logo from './assets/logo-horizontal.png';
 
 function FormPage() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!name || !email || !phone) {
-      alert("Por favor complete todos los campos.");
-      return;
-    }
-    const data = { name, email, phone };
-    axios
-      .get(
-        `https://sheet2api.com/v1/o9EZiCzTliju/leads-feria?phone=${phone}&email=${email}`
-      )
-      .then((response) => {
-        if (response.data.length > 0) {
-          alert(
-            "Este número de teléfono o correo electrónico ya está registrado."
-          );
-        } else {
-          axios
-            .post("https://sheet2api.com/v1/o9EZiCzTliju/leads-feria", data)
-            .then(() => {
-              alert("Sus datos han sido enviados correctamente.");
-              setName("");
-              setEmail("");
-              setPhone("");
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!name || !email || !phone) {
+            alert('Por favor complete todos los campos.');
+            return;
+        }
+        const data = { name, email, phone };
+        axios
+            .get(
+                `https://sheet2api.com/v1/o9EZiCzTliju/leads-feria?phone=${phone}&email=${email}`
+            )
+            .then((response) => {
+                if (response.data.length > 0) {
+                    alert(
+                        'Este número de teléfono o correo electrónico ya está registrado.'
+                    );
+                } else {
+                    axios
+                        .post(
+                            'https://sheet2api.com/v1/o9EZiCzTliju/leads-feria',
+                            data
+                        )
+                        .then(() => {
+                            alert('Sus datos han sido enviados correctamente.');
+                            setName('');
+                            setEmail('');
+                            setPhone('');
+                        })
+                        .catch((error) => {
+                            console.error(error);
+                        });
+                }
             })
             .catch((error) => {
-              console.error(error);
+                console.error(error);
             });
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
+    };
 
-  return (
-    <div className="container">
-      <h1>Formulario FERIA</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="name">Nombre:</label>
-          <input
-            type="text"
-            className="form-control"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
+    return (
+        <div className='formcito'>
+            <div className='form-container'>
+                <div className='logo-container'>
+                    <img src={Logo} alt='Logo' className='logo' />
+                </div>
+                <div className='form-wrapper'>
+                    <form onSubmit={handleSubmit}>
+                        <div className='form-group'>
+                            <label htmlFor='name'>Nombre:</label>
+                            <input
+                                type='text'
+                                className='form-control'
+                                id='name'
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                            />
+                        </div>
+                        <div className='form-group'>
+                            <label htmlFor='email'>Email:</label>
+                            <input
+                                type='email'
+                                className='form-control'
+                                id='email'
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </div>
+                        <div className='form-group'>
+                            <label htmlFor='phone'>Teléfono:</label>
+                            <input
+                                type='tel'
+                                className='form-control'
+                                id='phone'
+                                value={phone}
+                                onChange={(e) => setPhone(e.target.value)}
+                            />
+                        </div>
+                        <div className='form-group-button'>
+                            <button type='submit' className='botoncito'>
+                                Enviar
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
-        <div className="form-group">
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            className="form-control"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="phone">Teléfono:</label>
-          <input
-            type="tel"
-            className="form-control"
-            id="phone"
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)}
-          />
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Enviar
-        </button>
-      </form>
-    </div>
-  );
+    );
 }
 
 export default FormPage;
